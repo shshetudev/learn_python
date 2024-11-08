2130c42c07fd88c9e9df7c9b4a023ddbaecf3a20
shshetudev <shshetudev@gmail.com>
2024-11-08 08:29:40 +0600

    Our first lesson on python

---
+++ python-syntax/syntax.py
@@ -0,0 +1,22 @@
+print('Hello world!')
+
+# If statement
+if 5>2:
+    print("Five is greater than two!")
+
+# Variable
+num = 5  # number
+greetings = "Hello world!" # string
+
+print(num)
+print(greetings)    
+
+# Single Comment
+
+'''
+This is a multi line comment
+'''
+
+"""
+This is a multi line comment
+"""
---
+++ variable/variable.py
@@ -0,0 +1,7 @@
+num_str = str(3)
+int_num = int(3)
+float_num = float(3)
+
+print(num_str) # "3"
+print(int_num) # 3
+print(float_num) # 3.0
