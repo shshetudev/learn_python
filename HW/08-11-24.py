4c374f5c7880fb2388314b3e06b5c790760e12d6
Apkw1 <sha4babahmed@gmail.com>
2024-11-20 19:33:07 -0500

    commit

---
+++ HW/08-11-24.py
@@ -0,0 +1,107 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = str(3)
+y = int(3)
+z = float(3) 
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+print(x, y)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+x = 5
+print(type(x))
+
+x = 1 
+y = 2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = int(1)
+y = int(2.8)
+z = int("3")
+
+x = float(1)     
+y = float(2.8)   
+z = float("3")   
+w = float("4.2") 
