ddb6f53b08700245047271928b5d1e7e34231fce
Apkw1 <sha4babahmed@gmail.com>
2024-11-30 20:44:03 -0500

    assignment

---
+++ assignments/21-11-24.py
@@ -0,0 +1,80 @@
+print("Hello")
+print('Hello')
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+a = "Hello, World!"
+print(a[1])
+
+for x in "banana":
+  print(x)
+
+  a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+  txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+  print(bool("Hello"))
+print(bool(15))
+
+print(10 + 5)
+
+mylist = ["apple", "banana", "cherry"]
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+mytuple = ("apple", "banana", "cherry")
