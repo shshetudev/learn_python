ab7bce0050a3412a77c376a905d54ef70c71aa71
shshetudev <shshetudev@gmail.com>
2025-03-26 09:19:55 +0600

    Added some lines

--- assignments/march-2025/23-03-25/encapsulation/Encapsulation.py
+++ assignments/march-2025/23-03-25/encapsulation/Encapsulation.py
@@ -1,41 +1,41 @@
 class Book:
-    def __init__(self, title, author, isbn):
+    def __init__(self, title, author, isbn, available=False, borrower=None):
         self.__title = title
         self.__author = author
         self.__isbn = isbn
-        self.__available = True
+        self.__available = available
+        self.__borrower = borrower
     
     # Getters and setters for Book
     def get_title(self):
         return self.__title
-    
-    def set_title(self, title):
-        self.__title = title
 
     def get_author(self):
         return self.__author
     
-    def set_author(self, author):
-        self.__author = author
-    
     def get_isbn(self):
         return self.__isbn
-    
-    def set_isbn(self, isbn):
-        self.__isbn = isbn
     
     def get_available(self):
         return self.__available
     
-    def set_available(self, available):
+    def set_available(self, available=False):
         self.__available = available
+
+    def get_borrower(self):
+        return self.__borrower
+
+    def set_borrower(self, borrower=None):
+        self.__borrower = borrower
 
 
 class Library:
-    def __init__(self, name):
+    def __init__(self, name, books=[], max_borrow_limit=0, members={}):
+        self.__books = books
         self.__name = name
-        self.__books = []
-    
+        self.__max_borrow_limit = max_borrow_limit
+        self.__members = members
+
     # Getters and setters for Library
     def get_name(self):
         return self.__name
@@ -71,6 +71,13 @@ class Library:
                 return book
         return None
 
+    def is_book_exist_by_isbn(self, isbn):
+        book_existence_msg = "Book is not found"
+        for book in self.__books:
+            if book.get_isbn() == isbn:
+                book_existence_msg = "Book is found"
+        return book_existence_msg
+
 
 # Example Usage
 if __name__ == "__main__":
@@ -78,6 +85,10 @@ if __name__ == "__main__":
     library = Library("City Library")
     book1 = Book("Book1", "Author1", "1234567890")
     book2 = Book("Book2", "Author", "9876543210")
+
+    # Read only properties
+    childrenBook = Book("Three Little Pigs", "Author1", "1234567890")
+    childrenBook.set_borrower('Shabab')
     
     library.add_book(book1)
     library.add_book(book2)
@@ -88,3 +99,6 @@ if __name__ == "__main__":
     
     # Remove a Book
     library.remove_book("9876543210")
+
+    library.find_book_by_isbn("1234567890")
+    print(library.is_book_exist_by_isbn("1234567890"))
