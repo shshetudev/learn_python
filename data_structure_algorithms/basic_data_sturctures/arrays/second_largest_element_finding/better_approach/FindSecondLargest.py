31a2aa96b4499871b0c0e8df658d4be60f2e0830
shshetudev <shshetudev@gmail.com>
2025-05-28 09:15:49 +0600

    Adding better solution

---
+++ data_structure_algorithms/basic_data_sturctures/arrays/second_largest_element_finding/better_approach/FindSecondLargest.py
@@ -0,0 +1,23 @@
+# Time complexity = O(n)
+def getSecondLargest(arr):
+
+    n = len(arr)
+    # dummy largest and second largest value
+    largest = -1
+    secondLargest = -1
+
+    # largest
+    for i in range(n):
+        if(arr[i] > largest):
+            largest = arr[i]
+
+    # second largest
+    for i in range(n):
+        if(arr[i] > secondLargest and arr[i] != largest):
+            secondLargest = arr[i]
+
+    return secondLargest
+
+if __name__ == "__main__":
+    arr = [12, 35, 1, 10, 34, 1]
+    print(getSecondLargest(arr))
